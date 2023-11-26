13f2be247bd2136106f3279cd8dfabed30cb3ffa
oguzilgar <ouz1571@gmail.com>
2023-11-26 22:41:22 +0100

    cake update

--- cypress/e2e/Test/SearchTest.cy.js
+++ cypress/e2e/Test/SearchTest.cy.js
@@ -14,7 +14,7 @@ describe("Search Tests", () => {
 })
 
 it("Kullanici Greenkart sayfasina basariyla urun aramasi yapabilmeli", () => {
-    //const item = "Brocolli"
+    //const item = "Cake"
     const item = user.search.searchItem;
     searchPage.searchItem(item)
     cy.getByCompoundSel(LOCATORS.SEARCH_PAGE.PRODUCTS, LOCATORS.SEARCH_PAGE.PRODUCTS_NAME)
