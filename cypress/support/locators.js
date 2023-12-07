e256b15dd55837f0cde1db41d8f70cf7e8450bc8
staner70 <taner2003@gmail.com>
2023-12-07 14:24:10 +0100

    locators for login, signup and delete page created

--- cypress/support/locators.js
+++ cypress/support/locators.js
@@ -2,6 +2,56 @@ const LOCATORS = {
     EXAMPLE_PAGE: {
         EXAMPLE: ".example"
 
+    },
+    
+    HOME_PAGE: {
+        LOGIN_BTN: 'a[href="/login"]'
+    },
+
+    LOGIN_PAGE: {
+        SIGNUP_NAME: "signup-name",
+        SIGNUP_EMAIL: "signup-email",
+        SIGNUP_BTN: "signup-button",
+
+        LOGIN_EMAIL: "login-email",
+        LOGIN_PASSWORD: "login-password",
+        LOGIN_BTN: "login-button",
+
+        NEW_USER_TEXT:".signup-form > h2"
+
+    },
+
+    SIGNUP_PAGE: {
+        TITLE: "title",
+        NAME: "name",
+        PASSWORD: "password",
+        DAYS: "days",
+        MONTHS: "months",
+        YEARS: "years",
+        NEWSLETTER: "#newsletter",
+        PARTNERS: "#optin",
+        FIRST_NAME: "first_name",
+        LAST_NAME: "last_name",
+        COMPANY: "company",
+        ADDRESS1: "address",
+        ADDRESS2: "address2",
+        COUNTRY: "country",
+        STATE: "state",
+        CITY: "city",
+        ZIPCODE: "zipcode",
+        MOBILE_NUMBER: "mobile_number",
+        CREATE_ACCOUNT_BTN: "create-account",
+
+        ENTER_ACCOUNT_TEXT: ".login-form > h2  b",
+        ACCOUNT_CREATED: "account-created",
+        CONTINUE_BTN:"continue-button",
+
+        LOGGED_AS_TEXT:"#header li:nth-child(10) > a"
+    },
+    DELETE_PAGE:{
+        DELETE_ACCOUNT_BTN: "#header li:nth-child(5) > a",
+        ACCOUNT_DELETED_TEXT: "account-deleted",
+        CONTINUE_BTN :"continue-button"
     }
 }
 export default LOCATORS
