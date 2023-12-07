98b318ec64cacb69f8aa9fa537188115744efce6
staner70 <taner2003@gmail.com>
2023-12-07 14:30:32 +0100

    LoginPage and SignUpPage added

---
+++ cypress/e2e/Page/LoginPage.js
@@ -0,0 +1,14 @@
+import LOCATORS from "../../support/locators";
+
+class LoginPage {
+
+  signUp(user) {
+    cy.visit('/login')
+
+    cy.getByDataQa(LOCATORS.LOGIN_PAGE.SIGNUP_NAME).type(user.userInfo.userName);
+    cy.getByDataQa(LOCATORS.LOGIN_PAGE.SIGNUP_EMAIL).type(user.userInfo.email);
+    cy.getByDataQa(LOCATORS.LOGIN_PAGE.SIGNUP_BTN).click();
+  }
+}
+
+export default LoginPage;
---
+++ cypress/e2e/Page/SignupPage.js
@@ -0,0 +1,28 @@
+import LOCATORS from "../../support/locators";
+
+class SignupPage {
+  createAccount(user) {
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.TITLE).find("#id_gender1").check().should("be.visible");
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.PASSWORD).type(user.userInfo.password);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.DAYS).select(user.userInfo.dayOfBirth);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.MONTHS).select(user.userInfo.monthOfBirth);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.YEARS).select(user.userInfo.yearOfBirth);
+    cy.getBySel(LOCATORS.SIGNUP_PAGE.NEWSLETTER).check();
+    cy.getBySel(LOCATORS.SIGNUP_PAGE.PARTNERS).check();
+
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.FIRST_NAME).type(user.userInfo.firstName);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.LAST_NAME).type(user.userInfo.lastName);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.COMPANY).type(user.userInfo.company);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.ADDRESS1).type(user.userInfo.address1);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.ADDRESS2).type(user.userInfo.address2);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.COUNTRY).select(user.userInfo.country);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.STATE).type(user.userInfo.state);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.CITY).type(user.userInfo.city);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.ZIPCODE).type(user.userInfo.zipcode);
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.MOBILE_NUMBER).type(user.userInfo.mobilNumber);
+
+    cy.getByDataQa(LOCATORS.SIGNUP_PAGE.CREATE_ACCOUNT_BTN).click();
+  }
+}
+
+export default SignupPage;
