141566ee149b8fd36324a0cf14c574f470b07056
gulay <gulaysengulderen@gmail.com>
2023-12-09 17:51:40 +0100

    scrum-30 has been modified by Gulay

--- cypress/e2e/Page/TaseCasePage.js
+++ cypress/e2e/Page/TaseCasePage.js
@@ -1,10 +1,11 @@
 import LOCATORS from "../../support/locators"
 class TaseCase {
+    
     clickTaseCase(){
         cy.get(LOCATORS.TaseCase_PAGE.TaseCase).click()
        
-    
-    }
+     }
+   
     getTextTitle(){
         cy.get(LOCATORS.TaseCase_Text.TextTitle).should("be.visible")
     }
