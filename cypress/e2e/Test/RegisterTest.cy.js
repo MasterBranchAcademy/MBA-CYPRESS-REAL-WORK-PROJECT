e425aa63838477dffea3a1fad200a25c131c9ffb
karak <karakusbeykoz@gmail.com>
2023-12-10 13:50:24 +0100

    wrong message

--- cypress/e2e/Test/RegisterTest.cy.js
+++ cypress/e2e/Test/RegisterTest.cy.js
@@ -16,21 +16,21 @@ describe('Register Test', () => {
         })
     })
 
-   /*after(() => {
+    after(() => {
         cy.deleteAccount();
-    });*/
+    });
 
-      it('Kullanici Hesap Olusturabilmeli', () => {
+    it('Kullanici Hesap Olusturabilmeli', () => {
         homePage.visitPage()
-        cy.title().should('eq', user.home.title)        
+        cy.title().should('eq', user.home.title)
         cy.getBySel(LOCATORS.HOME_PAGE.LOGIN_BTN).click()
-        cy.getBySel(LOCATORS.LOGIN_PAGE.NEW_USER_TEXT).should('have.text', user.loginPage.newUserText)        
+        cy.getBySel(LOCATORS.LOGIN_PAGE.NEW_USER_TEXT).should('have.text', user.loginPage.newUserText)
         loginPage.signUp(user)
         cy.getBySel(LOCATORS.SIGNUP_PAGE.ENTER_ACCOUNT_TEXT).should('have.text', user.signUpPage.enterAccountText)
-        signupPage.createAccount(user)        
+        signupPage.createAccount(user)
         cy.getByDataQa(LOCATORS.SIGNUP_PAGE.ACCOUNT_CREATED).should('be.visible')
-        cy.getByDataQa(LOCATORS.SIGNUP_PAGE.CONTINUE_BTN).click()        
-        cy.getByCompoundSel(LOCATORS.HOME_PAGE.HEADER,LOCATORS.SIGNUP_PAGE.LOGGED_AS_TEXT).should('be.visible')
+        cy.getByDataQa(LOCATORS.SIGNUP_PAGE.CONTINUE_BTN).click()
+        cy.getByCompoundSel(LOCATORS.HOME_PAGE.HEADER, LOCATORS.SIGNUP_PAGE.LOGGED_AS_TEXT).should('be.visible')
     });
 
 
